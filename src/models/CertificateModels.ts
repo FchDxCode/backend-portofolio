5fce7f087e6d330a6d87102ec9252aa360f1044a
FchDxCode <fachru2006@gmail.com>
2025-05-18 22:03:39 +0700

    solve bug dropdown and add new models for certificate, create page certificate and full integrations crud

--- src/models/CertificateModels.ts
+++ src/models/CertificateModels.ts
@@ -1,14 +1,23 @@
+// models/CertificateModels.ts
+
 export interface Certificate {
-    id: number;
-    title?: Record<string, any>;
-    description?: Record<string, any>;
-    skill_id?: number; // skill id related to certificate
-    pdf?: string;
-    image?: string;
-    issued_by?: string;
-    issued_date?: string;
-    credential_id?: string;
-    valid_until?: string;
-    created_at?: string;
-    updated_at?: string;
-  }
+  id: number;
+  title?: Record<string, any>;
+  description?: Record<string, any>;
+  pdf?: string;
+  image?: string;
+  issued_by?: string;
+  issued_date?: string;
+  credential_id?: string;
+  valid_until?: string;
+  created_at?: string;
+  updated_at?: string;
+
+  skills?: number[];
+}
+
+// tambahhan table baru
+export interface CertificateSkillRelation {
+  certificate_id: number;
+  skill_id: number;
+}
